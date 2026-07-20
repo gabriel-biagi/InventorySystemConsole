a21d3622678ef45a46e887f1911d3e5799935f5c
gabriel-biagi <gabrielfelipebiagi@gmail.com>
2026-07-20 16:56:09 -0400

    feat: adiciona JsonEstoqueRepository com serialização e desserialização de estoque

---
+++ Persistence/JsonEstoqueRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using InventorySystem.Models;
+using System.Text.Json;
+
+namespace InventorySystem.Persistence
+{
+    public class JsonEstoqueRepository
+    {
+        private const string _caminho = "data/biblioteca.json";
+        public static void JsonSerializer(GestaoEstoque estoque)
+        {
+            string json = System.Text.Json.JsonSerializer.Serialize(estoque.ItensEstoque);
+            File.WriteAllText(_caminho, json);
+        }
+
+        public static List<Estoque>? JsonDeserialize()
+        {
+            string json = File.ReadAllText(_caminho);
+            return System.Text.Json.JsonSerializer.Deserialize<List<Estoque>>(json);
+        }
+    }
+}
